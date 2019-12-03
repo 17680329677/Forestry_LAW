4c66317439e0ebe26b2d93d979a76776de62201b
17680329677 <laobahepijiu@163.com>
2019-12-03 13:06:06 +0800

    complex content parsing and save to mysql code finish

--- fact_triple_extraction/complex_content_extraction/complex_analysis.py
+++ fact_triple_extraction/complex_content_extraction/complex_analysis.py
@@ -33,9 +33,17 @@ def xunfei_complex_analysis_and_save(contents):
                             parent_word = words_list[parent_index]
                         child_index = index
                         child_word = words_list[child_index]
-                        reletion_name = dp_tags[dp_info[index]['relate']]
+                        reletion_name = dp_tags[dp_info[index]['relate']] + '-' + dp_info[index]['relate']
                         # TODO: 存入对应数据库操作
-                        dp_result = [law_id, article_class, chapter_id, sentence_id, "".join(content_list), content, parent_word, reletion_name, child_word]
+                        dp_result = [law_id,                    # 0-法律id
+                                     article_class,             # 1-文章类型（1或2）
+                                     chapter_id,                # 章id
+                                     sentence_id,               # sentence_id
+                                     "".join(content_list),     # 完整句子
+                                     content,                   # 分析子句
+                                     parent_word,               # 头词
+                                     reletion_name,             # 关系
+                                     child_word]                # 尾词
                         save_to_dependency_parsing_result(dp_result)
                         print("%s -----(%s)---- %s" % (parent_word, reletion_name, child_word))
 
@@ -49,15 +57,26 @@ def xunfei_complex_analysis_and_save(contents):
                         sdp_child_index = sdgp_info[sdp_index]['id']
                         sdp_child_word = words_list[sdp_child_index]
                         if sdgp_info[sdp_index]['relate'] in sdp_tags:
-                            semantic_dp_name = sdp_tags[sdgp_info[sdp_index]['relate']]
+                            semantic_dp_name = sdp_tags[sdgp_info[sdp_index]['relate']] + '-' + sdgp_info[index]['relate']
                         elif str(sdgp_info[sdp_index]['relate']).startswith('r'):
                             main_relate = '' + str(sdgp_info[sdp_index]['relate'])[1:]
-                            semantic_dp_name = sdp_tags[main_relate] + '--反角色'
+                            semantic_dp_name = sdp_tags[main_relate] + '--反角色' + '-' + sdgp_info[index]['relate']
                         elif str(sdgp_info[sdp_index]['relate']).startswith('d'):
                             main_relate = '' + str(sdgp_info[sdp_index]['relate'])[1:]
-                            semantic_dp_name = sdp_tags[main_relate] + '--嵌套角色'
+                            semantic_dp_name = sdp_tags[main_relate] + '--嵌套角色' + '-' + sdgp_info[index]['relate']
                         else:
-                            semantic_dp_name = sdgp_info[sdp_index]['relate']
+                            semantic_dp_name = sdgp_info[sdp_index]['relate'] + '-' + sdgp_info[index]['relate']
+                        # TODO: 将语义依存分析结果存入数据库
+                        sdp_result = [law_id,
+                                      article_class,
+                                      chapter_id,
+                                      sentence_id,
+                                      "".join(content_list),
+                                      content,
+                                      sdp_parent_word,
+                                      semantic_dp_name,
+                                      sdp_child_word]
+                        save_to_semantic_dependency_result(sdp_result)
                         print("%s(%s)-----%s-----%s(%s)" % (
                             sdp_parent_word, postags_list[sdp_parent_index], semantic_dp_name, sdp_child_word,
                             postags_list[sdp_child_index]))
@@ -76,20 +95,87 @@ def xunfei_complex_analysis_and_save(contents):
                             role_lable_dict[verb].append(
                                 tuple((role_label['type'], "".join(words_list[begin: end + 1]))))
                     print(role_lable_dict)
-
+                    # TODO: 将语义角色标注信息存入数据库
+                    role_label_result = [law_id,
+                                         article_class,
+                                         chapter_id,
+                                         sentence_id,
+                                         "".join(content_list),
+                                         content,
+                                         role_lable_dict]
+                    save_to_semantic_role_label_result(role_label_result)
                     print('=========================================================================================')
 
 
 def save_to_dependency_parsing_result(result):
-    pass
+    insert_sql = '''insert into dependency_parsing_result (law_id, class, chapter_id, sentence_id, complete_sentence, 
+    parse_sentence, front_word, dependency_parsing_relation, tail_word) value (%s, %s, %s, %s, %s, %s, %s, %s, %s)'''
+    cursor = conn.cursor()
+    law_id = result[0]
+    article_class = result[1]
+    chapter_id = result[2]
+    sentence_id = result[3]
+    complete_sentence = result[4]
+    parse_sentence = result[5]
+    front_word = result[6]
+    dependency_parsing_relation = result[7]
+    tail_word = result[8]
+    try:
+        cursor.execute(insert_sql, (law_id, article_class, chapter_id, sentence_id, complete_sentence,
+                                    parse_sentence, front_word, dependency_parsing_relation, tail_word))
+        conn.commit()
+        print(sentence_id, parse_sentence, '----DEPENDENCY PARSING SUCCESS----')
+    except Exception as e:
+        conn.rollback()
+        print('\033[1;32;41m' + sentence_id + ': ' + parse_sentence + 'DP FAILED' + e + '\033[0m')
 
 
 def save_to_semantic_dependency_result(result):
+    insert_sql = '''insert into semantic_dependency_result (law_id, class, chapter_id, sentence_id, complete_sentence, 
+    parse_sentence, front_word, semantic_denpendency_relation, tail_word) value (%s, %s, %s, %s, %s, %s, %s, %s, %s)'''
+    cursor = conn.cursor()
+    law_id = result[0]
+    article_class = result[1]
+    chapter_id = result[2]
+    sentence_id = result[3]
+    complete_sentence = result[4]
+    parse_sentence = result[5]
+    front_word = result[6]
+    semantic_dependency_relation = result[7]
+    tail_word = result[8]
+    try:
+        cursor.execute(insert_sql, (law_id, article_class, chapter_id, sentence_id, complete_sentence,
+                                    parse_sentence, front_word, semantic_dependency_relation, tail_word))
+        conn.commit()
+        print(sentence_id, parse_sentence, '----SEMANTIC DEPENDENCY PARSING SUCCESS----')
+    except Exception as e:
+        conn.rollback()
+        print('\033[1;32;41m' + sentence_id + ': ' + parse_sentence + 'SDP FAILED' + e + '\033[0m')
     pass
 
 
 def save_to_semantic_role_label_result(result):
-    pass
+    insert_sql = '''insert into semantic_role_label_result (law_id, class, chapter_id, sentence_id, 
+    complete_sentence, parse_sentence, verb. role_label, content) value (%s, %s, %s, %s, %s, %s, %s, %s, %s)'''
+    cursor = conn.cursor()
+    law_id = result[0]
+    article_class = result[1]
+    chapter_id = result[2]
+    sentence_id = result[3]
+    complete_sentence = result[4]
+    parse_sentence = result[5]
+    role_label_dict = result[6]
+    for verb in role_label_dict:
+        for role_info in role_label_dict[verb]:
+            role = role_info[0]
+            content = role_info[1]
+            try:
+                cursor.execute(insert_sql, (law_id, article_class, chapter_id, sentence_id, complete_sentence, parse_sentence, verb, role, content))
+                conn.commit()
+                print(sentence_id, parse_sentence, '----SEMANTIC ROLE LABELLING SUCCESS----')
+            except Exception as e:
+                conn.rollback()
+                print('\033[1;32;41m' + sentence_id + ': ' + parse_sentence + 'SRL FAILED' + e + '\033[0m')
 
 
 if __name__ == '__main__':
