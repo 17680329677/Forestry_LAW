4c0047a073d8b654fd34d1f7c34fb502c4b33ff0
17680329677 <laobahepijiu@163.com>
2020-02-07 17:30:10 +0800

    forbid action extract

---
+++ fact_triple_extraction/forbid_relation_extract/forbid_relation_extraction.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+# coding=utf-8
+from data_resource import conn
+import re
+
+CURSOR = conn.cursor()
+
+
+def get_article_1_map_dict():        # 获取article_1中sentence、chapter与law之间的对应关系
+    query_article_1_sql = '''select id, chapter_id from article_1'''
+    query_chapter_sql = '''select id, law_id from chapter'''
+    CURSOR.execute(query_article_1_sql)
+    article_results = CURSOR.fetchall()
+    article_chapter_dict = dict()
+    for res in article_results:
+        article_chapter_dict.update({res[0]: res[1]})
+
+    CURSOR.execute(query_chapter_sql)
+    chapter_results = CURSOR.fetchall()
+    chapter_law_dict = dict()
+    for res in chapter_results:
+        chapter_law_dict.update({res[0]: res[1]})
+    return article_chapter_dict, chapter_law_dict
+
+
+# 通过对应的dict获取chapter_id，law_id
+def get_article_1_mapping(article_id, article_chapter_dict, chapter_law_dict):
+    chapter_id = article_chapter_dict[article_id]
+    law_id = chapter_law_dict[chapter_id]
+    return chapter_id, law_id
+
+
+def get_article_2_map_dict():
+    select_sql = '''select id, law_id from article_2'''
+    cursor = conn.cursor()
+    cursor.execute(select_sql)
+    results = cursor.fetchall()
+    article_2_map_dict = dict()
+    for res in results:
+        article_2_map_dict.update({res[0]: res[1]})
+    return article_2_map_dict
+
+
+def get_article_1_content():
+    select_sql = '''select * from article_1_sentence'''
+    CURSOR.execute(select_sql)
+    single_contents = CURSOR.fetchall()
+    article_chapter_dict, chapter_law_dict = get_article_1_map_dict()
+    result = []
+    forbid_pattern = "禁止(.*)"
+    for s_content in single_contents:
+        sentence_id = s_content[0]  # 获取sentence_id
+        article_1_id = s_content[1]  # 获取article_1_id
+        is_singel = s_content[2]
+        sentence_content = str(s_content[3]).strip()
+        pattern_res = re.search(forbid_pattern, sentence_content, re.M | re.I)
+        if pattern_res:
+            chapter_id, law_id = get_article_1_mapping(article_1_id, article_chapter_dict, chapter_law_dict)
+            result.append(tuple((law_id, 1, chapter_id, sentence_id, sentence_content, is_singel)))
+    return result
+
+
+def get_article_2_content():
+    select_sql = '''select * from article_2_sentence'''
+    CURSOR.execute(select_sql)
+    single_contents = CURSOR.fetchall()
+    article_2_map_dict = get_article_2_map_dict()
+    forbid_pattern = "禁止(.*)"
+    result = []
+    for s_content in single_contents:
+        sentence_id = s_content[0]  # 获取sentence_id
+        article_2_id = s_content[1]  # 获取article_2_id
+        is_singel = s_content[2]
+        sentence_content = str(s_content[3]).strip()
+        law_id = article_2_map_dict[article_2_id]
+        pattern_res = re.search(forbid_pattern, sentence_content, re.M | re.I)
+        if pattern_res:
+            result.append(tuple((law_id, 2, -1, sentence_id, sentence_content, is_singel)))
+    return result
+
+
+def forbid_relation_extract_core(sentence_list):
+    patter_1 = "(.*)内(.*)禁止(下列|以下)(.*)"
+    patter_2 = "禁止(下列|以下)(.*)的行为"
+    patter_3 = "禁止(.*)。"
+    forbid_list_1 = []
+    forbid_list_2 = []
+    forbid_list_3 = []
+    for sentence in sentence_list:
+        law_id = sentence[0]
+        article_type = sentence[1]
+        chapter_id = sentence[2]
+        sentence_id = sentence[3]
+        content = sentence[4]
+        is_single = sentence[5]
+        pattern_res_1 = re.search(patter_1, content, re.M | re.I)
+        pattern_res_2 = re.search(patter_2, content, re.M | re.I)
+        pattern_res_3 = re.search(patter_3, content, re.M | re.I)
+        if pattern_res_1:
+            # print(pattern_res_1.group(1))
+            if is_single == 1:
+                forbid_action = content.split('\n')[1:]
+                for act in forbid_action:
+                    # print(act)
+                    forbid_list_1.append(tuple((law_id, article_type, chapter_id, sentence_id, pattern_res_1.group(1), act)))
+            else:
+                if article_type == 1 or article_type == '1':
+                    select_sql = '''select clause_content from article_1_clause where article_1_sentence_id = %s'''
+                else:
+                    select_sql = '''select clause_content from article_2_clause where article_2_sentence_id = %s'''
+                CURSOR.execute(select_sql, (sentence_id,))
+                forbid_action = CURSOR.fetchall()
+                for act in forbid_action:
+                    # print(act[0])
+                    forbid_list_1.append(tuple((law_id, article_type, chapter_id, sentence_id, pattern_res_1.group(1), act[0])))
+        elif pattern_res_2:
+            # print("2--", pattern_res_2.group(2))
+            if is_single == 1:
+                forbid_action = content.split('\n')[1:]
+                for act in forbid_action:
+                    # print(act)
+                    forbid_list_2.append(tuple((law_id, article_type, chapter_id, sentence_id, pattern_res_2.group(2), act)))
+            else:
+                if article_type == 1 or article_type == '1':
+                    select_sql = '''select clause_content from article_1_clause where article_1_sentence_id = %s'''
+                else:
+                    select_sql = '''select clause_content from article_2_clause where article_2_sentence_id = %s'''
+                CURSOR.execute(select_sql, (sentence_id,))
+                forbid_action = CURSOR.fetchall()
+                for act in forbid_action:
+                    # print(act[0])
+                    forbid_list_2.append(tuple((law_id, article_type, chapter_id, sentence_id, pattern_res_2.group(2), act[0])))
+            pass
+        elif pattern_res_3:
+            # print("3--", pattern_res_3.group(1))
+            forbid_list_3.append(tuple((law_id, article_type, chapter_id, sentence_id, pattern_res_3.group(1))))
+    return forbid_list_1, forbid_list_2, forbid_list_3
+
+
+def forbid_act_save(forbid_list_1, forbid_list_2, forbid_list_3):
+    insert_sql_1 = '''insert into forbid_1 (law_id, chapter_id, sentence_id, forbid_subject, forbid_action) 
+                      value (%s, %s, %s, %s, %s)'''
+    insert_sql_2 = '''insert into forbid_2 (law_id, chapter_id, sentence_id, forbid_subject, forbid_action) 
+                          value (%s, %s, %s, %s, %s)'''
+    insert_sql_3 = '''insert into forbid_3 (law_id, chapter_id, sentence_id, forbid_action) value (%s, %s, %s, %s)'''
+    for forbid_act in forbid_list_1:
+        law_id = forbid_act[0]
+        chapter_id = forbid_act[2]
+        sentence_id = forbid_act[3]
+        forbid_subject = forbid_act[4]
+        forbid_action = forbid_act[5]
+        CURSOR.execute(insert_sql_1, (law_id, chapter_id, sentence_id, forbid_subject, forbid_action))
+        conn.commit()
+        print(forbid_act)
+
+    for forbid_act in forbid_list_2:
+        law_id = forbid_act[0]
+        chapter_id = forbid_act[2]
+        sentence_id = forbid_act[3]
+        forbid_subject = forbid_act[4]
+        forbid_action = forbid_act[5]
+        CURSOR.execute(insert_sql_2, (law_id, chapter_id, sentence_id, forbid_subject, forbid_action))
+        conn.commit()
+        print(forbid_act)
+
+    for forbid_act in forbid_list_3:
+        law_id = forbid_act[0]
+        chapter_id = forbid_act[2]
+        sentence_id = forbid_act[3]
+        forbid_action = forbid_act[4]
+        CURSOR.execute(insert_sql_3, (law_id, chapter_id, sentence_id, forbid_action))
+        conn.commit()
+        print(forbid_act)
+
+
+if __name__ == '__main__':
+    results_1 = get_article_1_content()
+    results_2 = get_article_2_content()
+    forbid_list_1, forbid_list_2, forbid_list_3 = forbid_relation_extract_core(results_2)
+    forbid_act_save(forbid_list_1, forbid_list_2, forbid_list_3)
--- fact_triple_extraction/response_to_explain/single_relation_subject_merge.py
+++ fact_triple_extraction/response_to_explain/single_relation_subject_merge.py
@@ -1,8 +1,34 @@
 #!/usr/bin/env python
 # coding=utf-8
 from data_resource import conn
+import re
 
 SINGLE_RELATION_CLASS = ['define', 'contain', 'duty', 'accord', 'right_and_obligations']
+
+
+def entity_wash():
+    cursor = conn.cursor()
+    chinese_pattern = "[\\u4e00-\\u9fa5\u3002\uff1b\uff0c\uff1a\u201c\u201d\uff08\uff09\u3001\uff1f\u300a\u300b]+"
+    for class_type in SINGLE_RELATION_CLASS:
+        table_name = class_type + '_relation'
+        select_sql = 'select * from %s' % table_name
+        cursor.execute(select_sql)
+        results = cursor.fetchall()
+        for relation in results:
+            id = relation[0]
+            subject = relation[4]
+            object = relation[6]
+            cn_pattern_res = re.search(chinese_pattern, subject, re.M | re.I)
+            if cn_pattern_res and cn_pattern_res.start() > 0 and cn_pattern_res.start() < 4:
+                subject = cn_pattern_res.group(0)
+            if subject[-1] == '，' or subject[-1] == '。' or subject[-1] == ',':
+                subject = subject[:-1]
+            if object[-1] == '，' or object[-1] == '。' or object[-1] == ',':
+                object = object[:-1]
+            update_sql = 'update %s ' % table_name + 'set subject = %s, object = %s where id = %s'
+            cursor.execute(update_sql, (subject, object, id))
+            conn.commit()
+            print(class_type, id, subject, object)
 
 
 def query_for_subject():
@@ -27,7 +53,46 @@ def subject_save(subject_set):
         cursor.execute(insert_sql, (subject,))
         conn.commit()
         print(subject)
+    print(len(subject_set))
+
+
+def scope_of_application_process():
+    select_scope_of_application = 'select * from scope_of_application'
+    select_right_and_obligation = 'select * from right_and_obligations_relation'
+    origin_law_list = []
+    new_list = []
+    cursor = conn.cursor()
+    cursor.execute(select_scope_of_application)
+    origin_results = cursor.fetchall()
+    cursor.execute(select_right_and_obligation)
+    new_results = cursor.fetchall()
+    for res in origin_results:
+        origin_law_list.append(res[1])
+    for res in new_results:
+        if '适用' in res[5]:
+            new_list.append(res[1])
+    origin_law_set = set(origin_law_list)
+    new_set = set(new_list)
+    print(len(origin_law_set), len(new_set))
+
+
+def accord_relation_process():
+    cursor = conn.cursor()
+    select_sql = 'select * from accord_relation'
+    cursor.execute(select_sql)
+    results = cursor.fetchall()
+    accord_list = []
+    for res in results:
+        relation = res[5]
+        for accord in str(relation).split('/'):
+            accord_list.append(accord)
+    accord_set = set(accord_list)
+    for accord in accord_set:
+        print(accord)
 
 
 if __name__ == '__main__':
-    subject_save(query_for_subject())
+    # entity_wash()
+    # subject_save(query_for_subject())
+    # scope_of_application_process()
+    accord_relation_process()
